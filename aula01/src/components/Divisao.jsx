f40c0fe5e546cf49d712865b7c0f1463553108de
PauloRenanB <paulorenanb@hotmail.com>
2024-08-27 13:35:38 -0300

    Adicionado componentes de adição, subtração, divisão e multiplicação

---
+++ aula01/src/components/Divisao.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Divisao = (props) => {
+  return (
+    <div>
+        <h1>
+            O resultado da divisão de num1 / num2 é: {props.num1 / props.num2}
+        </h1>
+    </div>
+  )
+}
+
+export default Divisao
---
+++ aula01/src/components/Subtracao.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Subtracao = (props) => {
+  return (
+    <div>
+        <h1>
+            O resultado da subtração de num1 - num2 é: {props.num1 - props.num2}
+        </h1>
+    </div>
+  )
+}
+
+export default Subtracao
